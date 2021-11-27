b92d9675217856df981c595beebf0b87eeb6ff8d
Ilya <mr.ilya868@mail.ru>
2021-11-27 14:08:49 +0300

    Implementation of classes

---
+++ project/include/chess.hpp
@@ -0,0 +1,39 @@
+#include <SFML/Graphics.hpp>
+
+namespace Chess {
+
+    class FigureTexture {
+        sf::Texture texture;
+
+    public:
+
+        FigureTexture() = default;
+        FigureTexture(std::string texture_file);
+        
+        sf::Texture get_texture();
+
+    };
+
+    class ChessFigure {
+        size_t x;
+        size_t y;
+
+    public:
+        ChessFigure(size_t new_x = 0, size_t new_y = 0);
+        
+        size_t get_x();
+        size_t get_y();
+    };
+
+    class Pawn: public ChessFigure, public FigureTexture {
+        std::vector<size_t> possible_positions_x;
+        std::vector<size_t> possible_positions_y;
+        sf::Sprite sprite;
+
+    public:
+        Pawn(FigureTexture texture);
+
+        void set_poositions(ChessFigure figure);
+    };
+    
+}  // namesapce Chess
---
+++ project/src/chess.cpp
@@ -0,0 +1,36 @@
+#include "chess.hpp"
+
+#define CELL_SIZE 50
+
+namespace Chess {
+
+    FigureTexture::FigureTexture(std::string texture_file) {
+        texture.loadFromFile(texture_file);
+    }
+
+    sf::Texture FigureTexture::get_texture() {
+            return texture;
+        }
+
+    ChessFigure::ChessFigure(size_t new_x = 0, size_t new_y = 0) {
+        x = new_x;
+        y = new_y;
+    }
+
+    size_t ChessFigure::get_x() { return x; }
+    size_t ChessFigure::get_y() { return y; }
+
+    Pawn::Pawn(FigureTexture texture) {
+        sprite.setTexture(texture.get_texture());
+        sprite.setTextureRect(sf::IntRect(0, 0, CELL_SIZE, CELL_SIZE));
+    }
+
+    void Pawn::set_poositions(ChessFigure figure) {
+        possible_positions_x.push_back(figure.get_x() + CELL_SIZE);
+        possible_positions_y.push_back(figure.get_y() - CELL_SIZE);
+
+        possible_positions_x.push_back(figure.get_x() - CELL_SIZE);
+        possible_positions_y.push_back(figure.get_y() - CELL_SIZE);
+    }
+    
+}  // namesapce Chess
