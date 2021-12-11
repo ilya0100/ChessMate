137cf3dc65ba28c1e337e3598efb4fa12f49cd82
Khasbulat <khas.2015@gmail.com>
2021-12-11 16:21:44 +0300

    tempfile

---
+++ project/src/temp.cpp
@@ -0,0 +1,1 @@
+if (sf::IntRect(figures_arr[i].getSpritePos().x * windowRatio.x, figures_arr[i].getSpritePos().y * windowRatio.y, (SPRITE_SIZE * SCALE_FACTOR) * windowRatio.x, (SPRITE_SIZE * SCALE_FACTOR) * windowRatio.y).contains(pos.x, pos.y)) {
